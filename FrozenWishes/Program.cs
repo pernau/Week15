949231a65078021732d8522b6305ede9aa4924ae
Heleri Pernau <hpernau@tktk.ee>
2021-12-29 11:25:57 +0200

    add new projects

---
+++ FrozenWishes/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FrozenWishes
+{
+    class Program
+    {
+        class Wish
+        {
+            string name;
+            string gift;
+
+            public Wish(string _name, string _gift)
+            {
+                name = _name;
+                gift = _gift;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+            public string Gift
+            {
+                get { return gift; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Wish> frozenWishes = new List<Wish>();
+            string[] wishesFromFile = GetDataFromFile();
+
+            foreach(string line in wishesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Wish newWish = new Wish(tempArray[0], tempArray[1]);
+                frozenWishes.Add(newWish);
+            }
+
+            foreach(Wish wishesFromList in frozenWishes)
+            {
+                Console.WriteLine($"Name ---> {wishesFromList.Name}. Gift ---> {wishesFromList.Gift}");
+            }
+
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\Heleri\samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+
+}
+
+    
